30ab8f2528585e55204f0a60f697f62bac158351
joanfonseca <joan.fonseca01@uptc.edu.co>
2021-08-30 19:50:35 -0500

    Creado con el fin de evitar la confusion de rutas no definidas

---
+++ src/app/modules/common/notfount/notfount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notfount',
+  templateUrl: './notfount.component.html',
+  styleUrls: ['./notfount.component.scss']
+})
+export class NotfountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
