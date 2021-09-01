78d2056a7563df5a80c4eeaf7c0d682087963022
joanfonseca <joan.fonseca01@uptc.edu.co>
2021-09-01 07:45:19 -0500

    Agregando url aws cs2 coneccion al back

--- src/app/models/global/global.ts
+++ src/app/models/global/global.ts
@@ -1,7 +1,10 @@
 export var Global = {
     /** Servidor */
     //----Pruebas
-    apiBackConnect: "http://localhost:8080/",
+ //   apiBackConnect: "http://localhost:8080/",
+   //produccion
+    apiBackConnect: "http://ec2-52-90-251-196.compute-1.amazonaws.com:8080/",
+  
   
     /** Organizacion */
     organization_name: "School",
